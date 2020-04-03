2b39cd5356b8eb7040d965474cd528bac6d2cf51
Ubuntu <maldo2212@Ubuntu16.dejryo1pd53ulh22lh2ougavqc.gx.internal.cloudapp.net>
2020-04-03 21:46:04 +0000

    added ruby file directory

---
+++ ruby/file1.rb
@@ -0,0 +1,12 @@
+
+array1 = ["Tycho", "Tame Impala", "ZHU", "Bassnectar", "Luttrell"]
+puts "The best music artists of my generation are: "
+puts " "
+for array_element in array1
+          puts array_element
+end
+TIME = [Time.now]
+puts " "
+puts "The current time is: "
+puts TIME
+puts " "
